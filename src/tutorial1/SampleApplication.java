a0ea7bee37f29bb785302845c8e1be9fbee8b46f
belle09 <angeliqueumilda1009@gmail.com>
2020-10-14 19:28:25 +0800

    added strings

--- src/tutorial1/SampleApplication.java
+++ src/tutorial1/SampleApplication.java
@@ -5,10 +5,17 @@ public class SampleApplication {
 	public static void main(String[] args) {
 		int myNumber;
 		int mySecondNumber;
-
+		
+		String sampleText = "Hello ";
+		String secondText = "How are you? ";
+		String thirdText = "I'm Good ";
+		String greetingText = sampleText + secondText + thirdText;
+		
 		myNumber = 88;
 		mySecondNumber = 99;
 		System.out.println("The total is: " + myNumber + mySecondNumber);
+		System.out.println(sampleText + ", " + secondText + " " + thirdText + ".");
+		System.out.println(greetingText);
 	}
-
+	
 }
