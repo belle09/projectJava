833ab5bb16b5dca20c6f0dfd02300d434507f25e
belle09 <angeliqueumilda1009@gmail.com>
2020-10-20 15:19:34 +0800

    updated errors on sampleApp

--- src/tutorial1/SampleApplication.java
+++ src/tutorial1/SampleApplication.java
@@ -6,30 +6,24 @@ public class SampleApplication {
 		int myNumber;
 		int mySecondNumber;
 		
-<<<<<<< HEAD
 		myNumber = 88;
 		mySecondNumber = 99;
 		
-=======
->>>>>>> development
 		String sampleText = "Hello ";
 		String secondText = "How are you? ";
 		String thirdText = "I'm Good ";
 		String greetingText = sampleText + secondText + thirdText;
-<<<<<<< HEAD
 			
 		System.out.println("The total is: " + myNumber + mySecondNumber);
 		System.out.println(sampleText + ", " + secondText + " " + thirdText + ".");
 		System.out.println(greetingText);
 		
-=======
 		
 		myNumber = 88;
 		mySecondNumber = 99;
 		System.out.println("The total is: " + myNumber + mySecondNumber);
 		System.out.println(sampleText + ", " + secondText + " " + thirdText + ".");
 		System.out.println(greetingText);
->>>>>>> development
 	}
 	
 }
