e1166b35e8755e3c4dcd27bd533fd84baa923d2d
belle09 <angeliqueumilda1009@gmail.com>
2020-10-27 00:22:18 +0800

    arrays

--- src/tutorial1/arraySampleTraining.java
+++ src/tutorial1/arraySampleTraining.java
@@ -4,7 +4,8 @@ public class arraySampleTraining {
 
 	public static void main(String[] args) {
 
-		int[] numbers = {0,1,3,5,7,9,11,13,15};
+		// iterating through an array: integer
+		/*int[] numbers = {0,1,3,5,7,9,11,13,15};
 		int sum = 0;
 		
 		for(int i=0; i<numbers.length; i++) {
@@ -13,8 +14,33 @@ public class arraySampleTraining {
 				sum = i + sum;
 				
 				System.out.println("Total: " + sum);
-		} // end for
+		} // end for  
 		
+		
+		// another iteration of array: String
+		String[] phones = {"Apple","Samsung","Sony","Nokia"};
+		
+		for(String phone: phones) {
+			System.out.println("\nMobile Unit: " + phone);
+		} // end for */
+		
+		
+		// Two arrays, one has data while another has none
+		int[] id = {100,101,102,103};
+		int[] grades = {};
+		
+			
+			for(int sample = 0; sample<1; sample++) {
+				System.out.println("id: " + sample);
+				
+				for(int newsample: id) {
+					
+					System.out.println("grades: " + newsample);
+				} // inner for
+				
+			} // end for
+
+			
 	} // end main
 
 } // end class
